7b65a6a946d7802ee66c33880a0e3550b83fa1ac
OmKaRSaHoO20 <omkarsahoo@eng.rizvi.edu.in>
2024-05-31 23:25:21 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [trends, setTrends] = useState([]);
+  const [timestamp, setTimestamp] = useState('');
+  const [ipAddress, setIpAddress] = useState('');
+  const [jsonData, setJsonData] = useState({});
+  const [showResults, setShowResults] = useState(false);
+  const [ButtonLoading, setButtonLoading] = useState(false);
+
+  const handleClick = () => {
+    setShowResults(false);
+    setButtonLoading(false);
+    fetch('http://127.0.0.1:5000/api/trending_topics')
+      .then(response => response.json())
+      .then(data => {
+        setTrends(data.Topics.map(topic => topic.Topic));
+        setTimestamp(data.Time);
+        setIpAddress(data.IP);
+        setJsonData(data);
+        setShowResults(true);
+        setButtonLoading(true);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        showResults ?
+          <header className="App-header">
+            <button onClick={handleClick}>Get Trending Topics</button>
+            {showResults && (
+              <div>
+                <p>These are the most happening topics as on {timestamp}</p>
+                <ul>
+                  {trends.map((trend, index) => (
+                    <li key={index}>{trend}</li>
+                  ))}
+                </ul>
+                <p>The IP address used for this query was {ipAddress}</p>
+                <p>Here’s a JSON extract of this record from the MongoDB:</p>
+                <pre>{JSON.stringify(jsonData, null, 2)}</pre>
+                <p>
+                  <button onClick={() => handleClick()}>Click here to run the query again</button>
+                </p>
+              </div>
+            )}
+          </header>
+          :
+          <>
+            {
+              ButtonLoading ?
+                <>LOADING....</>
+                :
+                <button onClick={handleClick}>Get Trending Topics</button>
+            }
+          </>
+      }
     </div>
   );
 }
